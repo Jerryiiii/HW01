536933e570805a0120dc4e3f55f44731481ee037
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-09-13 10:38:01 -0700

    Update puzzle-3.js

--- puzzle-3.js
+++ puzzle-3.js
@@ -1,12 +1,12 @@
-down
-down
-save getColor as firstColorSeen
-down
-down
-down
-right
-right
-setColor remember firstColorSeen
-right
-right 
-up
+down()
+down()
+var blue = getColor()
+down()
+down()
+down()
+right()
+right()
+setColor(blue)
+right()
+right()
+up()
