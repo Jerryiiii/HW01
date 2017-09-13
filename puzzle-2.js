7d5c6ee6fbf6aa292e3c82144c5092ef8f8eef88
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-09-13 10:37:35 -0700

    Update puzzle-2.js

--- puzzle-2.js
+++ puzzle-2.js
@@ -1,14 +1,15 @@
-save 0 as steps 
-repeat if steps < 6
-| down
-| save steps + 1 as steps 
-right
-right 
-right 
-up
-up
-right 
-right 
-down
-down 
-down
+var steps = 0 
+while (steps < 6) {
+down()
+steps = steps + 1
+}
+right()
+right()
+right()
+up()
+up()
+right()
+right()
+down()
+down()
+down()
