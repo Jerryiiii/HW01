ec08267badb5b326aeb59a93943faa453639cd91
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-09-13 10:39:56 -0700

    Update puzzle-7.js

--- puzzle-7.js
+++ puzzle-7.js
@@ -1,20 +1,23 @@
-recipe pinkRight
-| if get Color is pink
-   | right
-   | right
-recipe downToPink
-| repeat if getColor isn’t pink
-   | down
-recipe upToPink
-| repeat if getColor isn’t pink
-   | up
-downToPink
-pinkRight
-upToPink
-pinkRight
-downToPink
-pinkRight
-upToPink
-pinkRight
-downToPink
-right
+function redR(){
+  if (getColor()=="red"){
+    right()
+    right()}
+}
+function downToRed(){
+  while(getColor()!="red"){
+    down()}
+}
+function upToRed(){
+  while(getColor()!="red"){
+    up()}
+}
+downToRed()
+redR()
+upToRed()
+redR()
+downToRed()
+redR()
+upToRed()
+redR()
+downToRed()
+right()
